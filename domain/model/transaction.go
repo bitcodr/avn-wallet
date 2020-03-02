797a2b74ee3786bce45940ebc4bdddf1f463d973
amiralii <mr.roshanae@gmail.com>
2020-03-02 11:06:22 +0330

    fix bug

--- domain/model/transaction.go
+++ domain/model/transaction.go
@@ -5,8 +5,7 @@ import "github.com/google/uuid"
 type Transaction struct {
 	ID          uuid.UUID `json:"id" msgpack:"id" validate:"required"`
 	Type        string    `json:"type" msgpack:"type" validate:"required"`
-	Status      string    `json:"status" msgpack:"status" validate:"required"`
-	Description string    `json:"description" msgpack:"status" validate:"required"`
+	Description string    `json:"description" msgpack:"description" validate:"required"`
 	CreatedAt   string    `json:"createdAt" msgpack:"createdAt"`
 	Cause       string    `json:"cause" msgpack:"cause"`
 	CauseTimes  uint32    `json:"causeTimes" msgpack:"causeTimes"`
--- domain/model/user.go
+++ domain/model/user.go
@@ -10,7 +10,6 @@ type User struct {
 	FirstName    string         `json:"firstName" msgpack:"firstName"`
 	LastName     string         `json:"lastName" msgpack:"lastName"`
 	Email        string         `json:"email" msgpack:"email"`
-	Status       string         `json:"status" msgpack:"status"`
 	CreatedAt    string         `json:"createdAt" msgpack:"createdAt"`
 	UpdatedAt    string         `json:"updatedAt" msgpack:"updatedAt"`
 	Wallet       *Wallet        `json:"wallet" msgpack:"wallet"`
